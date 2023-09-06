6dcf67a0de4175db129fdbb4f280773fe53675b3
shakib2205 <shakibur35-2737@diu.edu.bd>
2023-09-06 13:26:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route  path= '/about' element={<About></About>}></Route>
+        <Route path ='/products' element={<Products></Products>}></Route>
+        
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>Lots of products here</h2>
+        </div>
+    );
+};
+
+export default Products;
